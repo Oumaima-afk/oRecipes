bc1a2a736f675b99d564098e88f4e481c52f2eb7
Oumaima-afk <oumaima_afakkir@hotmail.com>
2025-09-03 10:29:30 +0200

    css improved

--- front/src/pages/ErrorPage.tsx
+++ front/src/pages/ErrorPage.tsx
@@ -1,10 +1,19 @@
 import "./ErrorPage.css";
+import { Link } from "react-router";
 
 export default function ErrorPage() {
   return (
     <main className="error-page">
-      <h2 className="error-page-title">Page non trouvée</h2>
-      <p>404</p>
+      <div className="error-page-content">
+        <h2 className="error-page-title">Page non trouvée</h2>
+        <div className="error-page-code">404</div>
+        <p className="error-page-message">
+          Oups ! Il semble que la page que vous cherchez n'existe pas...
+        </p>
+        <Link to="/" className="error-page-button">
+          Retour à l'accueil
+        </Link>
+      </div>
     </main>
   );
 }
